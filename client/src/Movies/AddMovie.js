bf05a0a6917e322799eb2c80d045d194ff77536b
Jessica <duellje@oregonstate.edu>
2020-07-22 16:12:00 -0400

    added add movie form

--- client/src/Movies/AddMovie.js
+++ client/src/Movies/AddMovie.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const AddMovie = () => {
+    const newMovie = {
+        title: '',
+        director: '',
+        metascore: ''
+    }
+
+    const [movie, setMovie] = useState(newMovie)
+
+    const handleChange = event => {
+
+    }
+
+    const submitMovie = event => {
+
+    }
+
     return (
-        <div>
-        </div>
+        <form onSubmit={submitMovie}>
+            <label htmlFor="title">
+                Title: &nbsp;
+                <input type="text" id ="title" name="title" value={movie.title}  onChange={handleChange} />
+            </label>
+            <label htmlFor="director">
+                Director: &nbsp;
+                <input type="text" id="director" name="director" value={movie.director} onChange={handleChange}  />
+            </label>
+            <label htmlFor="metascore">
+                Metascore: &nbsp;
+                <input type="number" id="metascore" name="metascore" value={movie.metascore} onChange={handleChange} step="1" />
+            </label>
+            <button>Edit Movie</button>
+        </form>
     )
 }
 
